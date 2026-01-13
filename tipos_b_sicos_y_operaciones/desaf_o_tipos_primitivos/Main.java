1010130c49eda6083316bb1a690084af609799b4
samu-cripto <>
2026-01-13 19:27:47 +0000

    Desafío: Tipos Primitivos

---
+++ tipos_b_sicos_y_operaciones/desaf_o_tipos_primitivos/Main.java
@@ -0,0 +1,13 @@
+package com.example;
+
+public class Main {
+    public static void main(String[] args) {
+        byte myNum = 14;
+        double myFractional = 14.928;
+        short myBiggerNum = -710;
+        char myLetter = 'B';
+        boolean myBool = true;
+        float myWho = 1.74f;
+        System.out.println("It's working, well done!");
+    }
+}
