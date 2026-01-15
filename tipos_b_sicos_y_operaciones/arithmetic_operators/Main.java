603f4cb79179e58295e302aef2fd543e897da660
samu-cripto <>
2026-01-15 21:46:53 +0000

    Arithmetic Operators

---
+++ tipos_b_sicos_y_operaciones/arithmetic_operators/Main.java
@@ -0,0 +1,13 @@
+package com.example;
+
+public class Main {
+    public static void main(String[] args) {
+        int firstNumber = 9;
+        int secondNumber = 4;
+        double thirdNumber = 2.0;
+        int suma = firstNumber+secondNumber;
+        double divicion = suma/ thirdNumber;
+        double result = divicion;
+        System.out.println(result);
+    }
+}
